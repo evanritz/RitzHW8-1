4eb7fa3f11e16a76502941b372ae1501067bf501
evanritz <evanfwritz@gmail.com>
2021-11-09 16:29:43 +0000

    Finished

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+echo "Configuring GPIO pins..."
+config-pin -a p9.12 out
+config-pin -a p8.16 in+ 
+echo "Configured GPIO pins"
+echo "Compiling control_GPIO.cpp..."
 g++ -Wall control_GPIO.cpp -o control_GPIO
+echo "Finished"
